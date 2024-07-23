a0b2ce56e528c4f16f42dae792dc6865fbecfacd
Rafael G. O. Viana <rafael.viana@ufms.br>
2024-07-23 15:33:17 -0400

    up

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ongRoutes = require('./routes/ong');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000; 
