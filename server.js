19ac5ff07cff34b2b2461bdc7baec0838b698bff
Rafael G. O. Viana <rafael.viana@ufms.br>
2024-07-26 18:20:12 -0400

    up

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-  res.json({ message: "Bem-vindo à API",
+  res.json({ message: "Bem-vindo à API ONGs Senac 145",
     email:"contato@viana.dev"
   })
 
